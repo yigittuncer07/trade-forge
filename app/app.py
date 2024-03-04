a283d738912a18ac4d5f66caf865a020f2d4bdfe
yigittuncer07 <yigit_tuncer03@hotmail.com>
2024-03-04 23:05:48 +0300

    added non-functional pages

--- app/app.py
+++ app/app.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/signin.html')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ app/static/js/scipt.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    button = document.getElementById("get-started-btn");
+    button.addEventListener("click", function() {
+        alert("Button clicked!");
+    });
+}
