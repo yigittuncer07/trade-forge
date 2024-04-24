a3bd7b6914cb289f83aaf231ba163f6157a3023e
Hasan Pekedis <hasanpekedis365@hotmail.com>
2024-04-25 00:50:55 +0300

    basic fix

--- app/app.py
+++ app/app.py
@@ -2,19 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/index.html')
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/signin.html')
+@app.route('/signin')
 def signin():
     return render_template('signin.html')
 
-@app.route('/signup.html')
+@app.route('/signup')
 def signup():
     return render_template('signup.html')
 
-@app.route('/about.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
