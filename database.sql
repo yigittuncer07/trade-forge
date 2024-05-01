4f19a0388a0c463c3b322aa028dc73a001a9c394
Hasan Pekedis <hasanpekedis365@hotmail.com>
2024-05-01 18:19:21 +0300

    database update

--- database.sql
+++ database.sql
@@ -11,11 +11,9 @@ CREATE TABLE IF NOT EXISTS Users (
 );
 
  
-USE tradeforge;
+CREATE TABLE cryptos (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255)
+);
 
--- Inserting user data
-INSERT INTO Users (UserName, UserSurname, PasswordHash, Email, SocialId) 
-VALUES 
-('John', 'Doe', 'd30318c3c2311ac757c0b740a14957fe', 'john@example.com', '12A3B4R5F6'),
-('Alice', 'Smith', '082949a8dfacccda185a135db425377b', 'alice@example.com', '65D432DSA1'),
-('Bob', 'Johnson', '583317fe343d4ecca20295776885a929', 'bob@example.com', '9W7E65UR4');
+INSERT INTO cryptos (Name) VALUES ('Bitcoin'), ('Ethereum'), ('Monero');
